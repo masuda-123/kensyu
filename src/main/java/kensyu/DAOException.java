a4c4217ac6752ab2a889d3100a6accad733c38ab
masuda <r_masuda@emyus.jp>
2024-04-01 16:41:08 +0900

    DAOExceptionクラスを作成

---
+++ src/main/java/kensyu/DAOException.java
@@ -0,0 +1,13 @@
+package kensyu;
+
+/** Exceptionを基にDAOExceptionを拡張する**/
+public class DAOException extends Exception {
+
+	/** 引数付きのコンストラクタ呼び出し **/
+	public DAOException(String message) {
+
+		super(message);
+
+	}
+
+}
