5bfb53518464acabca1440f041300c67faf6e1da
masuda <r_masuda@emyus.jp>
2024-04-19 13:59:04 +0900

    Logout.javaを作成し、セッションの削除と画面遷移の処理を追加。

---
+++ src/main/java/Logout.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	    HttpSession session = request.getSession(false);
+	    // セッションが存在する場合は一度破棄する
+	    if (session != null) {
+	      session.invalidate();
+	    }
+	    //ログイン画面に遷移させる
+		RequestDispatcher rd = request.getRequestDispatcher("/Login.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
