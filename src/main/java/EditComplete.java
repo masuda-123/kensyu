ac06a0512c82f7be9229f138d6caa909578ea8a0
masuda <r_masuda@emyus.jp>
2024-04-24 18:04:42 +0900

    EditComplete.javaにコメントを追加

--- src/main/java/EditComplete.java
+++ src/main/java/EditComplete.java
@@ -42,43 +42,51 @@ public class EditComplete extends Base {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
 		
+		//Baseクラスでログインしているかどうかを確認
 		if (super.isCheckLogin(request, response)) {
-			return ;
+			return ; //trueだった場合return
 		}
-		
 		try {
+			//QuestionsDaoオブジェクトを作成
 			QuestionsDao queDao = new QuestionsDao();
+			//CorrectAnswersDaoオブジェクトを作成
 			CorrectAnswersDao ansDao = new CorrectAnswersDao(); 
-			
+			//フォームから渡された値を、変数questionに格納
 			String question = request.getParameter("question");
+			//フォームから渡された値を、配列answersに格納
 			String[] answers = request.getParameterValues("answer");
+			//フォームから渡された値を、int型に変換して変数questionIdに格納
 			int questionId = Integer.parseInt(request.getParameter("questionId"));
+			//フォームから割らされた値を、int型に変換して、配列answersIdに格納
 			int[] answersId =  Stream.of(request.getParameterValues("answerId")).mapToInt(Integer::parseInt).toArray();
 			
-			//questionの更新
+			///update_questionメソッドを呼び出して、問題を更新
 			queDao.update_question(question, questionId);
 			
-			//既存の答えのデータを取得
+			///search_questions_idメソッドを呼び出して、questionIdと一致するレコードを取得し、既存の答えのデータを取得
 			ArrayList<CorrectAnswersBean> ansList = ansDao.search_questions_id(questionId);
 			
+			//フォームから渡された答えの数だけ処理を繰り返す
 			for(int i = 0; i < answers.length; i++) {
-				if( i < answersId.length) { //更新された答えがあった場合
-					ansDao.update_answer(answersId[i], answers[i]); //answerの更新
-				} else { //新たに追加された答えがあった場合
-					ansDao.register_answer(questionId, answers[i]); //answerの登録
+				if( i < answersId.length) { //フォームから渡された答えの中に、idを持つものがあった場合（更新された答えがあった場合）
+					ansDao.update_answer(answersId[i], answers[i]); //update_answerメソッドを呼び出して、答えを更新
+				} else { //idを持たない答えがあった場合（新たに追加された答えがあった場合）
+					ansDao.register_answer(questionId, answers[i]); //register_answerメソッドを呼び出して、答えを登録
 				}
 			}
-			if(ansList.size() > answersId.length) { //削除された答えがあった場合
-				for(CorrectAnswersBean ans : ansList) {
-					if(!(Arrays.stream(answersId).anyMatch(x -> x == ans.getId()))){
-						ansDao.delete_answer(ans.getId()); //answerの削除
+			if(ansList.size() > answersId.length) { //既存の答えの数の方が、フォームから渡されたidの数より多かった場合（削除された答えがあった場合）
+				for(CorrectAnswersBean ans : ansList) { //既存の答えの数だけ、処理を繰り返す
+					if(!(Arrays.stream(answersId).anyMatch(x -> x == ans.getId()))){ //既存の答えにしかないidがあった場合
+						ansDao.delete_answer(ans.getId()); //delete_answerメソッドを呼び出して、答えを削除
 					}
 				}
 			}
-			
+			//./ListのURLにリダイレクトする
 			response.sendRedirect("./List");
 			return;
+		//try文の中で例外が発生した場合、catch句に処理が移る
 		} catch (Exception e) {
+			//スタックトレースを出力する
 			e.printStackTrace();
 		}
 	}
