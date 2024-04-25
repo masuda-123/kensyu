05ed8cbb203d7c728811f137e7638b49c9513762
masuda <r_masuda@emyus.jp>
2024-04-25 11:21:02 +0900

    Delete_confirm.javaにコメントを追加

--- src/main/java/Delete_confirm.java
+++ src/main/java/Delete_confirm.java
@@ -33,34 +33,44 @@ public class Delete_confirm extends Base {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//Baseクラスでログインしているかどうかを確認
 		if (super.isCheckLogin(request, response)) {
-			return ;
+			return ; //trueだった場合return
 		}
 		
 		try {
 			//URLパラメータからquestionIdを取得
 			int questionId = Integer.parseInt(request.getParameter("id"));
-				
+			//QuestionsDaoオブジェクトを作成
 			QuestionsDao queDao = new QuestionsDao();
-			//questionsテーブルからquestionIdが一致するレコードを取得
+			//search_idメソッドを呼び出して、questionIdと一致するレコードを取得
 			QuestionsBean question = queDao.search_id(questionId);
+			//リクエストスコープへquestionを格納
 			request.setAttribute("question", question.getQuestion());
+			//リクエストスコープへquestionIdを格納
 			request.setAttribute("questionId", questionId);
-				
+			//CorrectAnswersDaoオブジェクトを作成
 			CorrectAnswersDao ansDao = new CorrectAnswersDao();
-			//correct_answersテーブルからquestionIdが一致するレコードを取得
+			//search_question_idメソッドを呼び出して、questionIdと一致するレコードを取得
 			ArrayList<CorrectAnswersBean> ansList = ansDao.search_questions_id(questionId);
-			//レコードからanswerを取得し、配列に格納
+			//ansListを格納する配列を宣言
 			String[] answers = new String[ansList.size()];
+			//ansListの要素数分処理を繰り返す
 			for(int i = 0; i < ansList.size(); i++) {
+				//ansListの中のanswerを配列に格納
 				answers[i] = ansList.get(i).getAnswer();
 			}
+			//リクエストスコープへanswersを格納
 			request.setAttribute("answers", answers);
-				
+			//画面の遷移先としてDelete_confirm画面を定義
 			RequestDispatcher rd = request.getRequestDispatcher("/Delete_confirm.jsp");
+			//foward(...)で定義された転送先に処理が移る
 			rd.forward(request, response);
 			return;
+		//try文の中で例外が発生した場合、catch句に処理が移る
 		} catch (Exception e) {
+			//スタックトレースを出力する
 			e.printStackTrace();
 		}
 	}
