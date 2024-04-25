5fbc371ab1389bc99cd7bf0b949c56bfda5af9f4
masuda <r_masuda@emyus.jp>
2024-04-25 13:42:10 +0900

    Reuslt.java にコメントを追加

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -49,48 +49,64 @@ public class Result extends Base {
 		if (super.isCheckLogin(request, response)) {
 			return ; //trueだった場合return
 		}
-		
-		int[] questions_id = Stream.of(request.getParameterValues("questions_id")).mapToInt(Integer::parseInt).toArray();
+		//フォームから渡された値を、int型の配列に変換して、配列questionsIdに格納
+		int[] questionsId = Stream.of(request.getParameterValues("questions_id")).mapToInt(Integer::parseInt).toArray();
+		//フォームから渡された値を、配列answersに格納
 		String[] answers = request.getParameterValues("answer");
 		
 		try {
 			//正解の問題数を格納する変数
 			int correctQueCnt = 0;
-			for(int i = 0; i < questions_id.length; i++) {
-				CorrectAnswersDao a_dao = new CorrectAnswersDao();
-				//入力された答えと一致するレコードを全て取得
-				ArrayList<CorrectAnswersBean> a_list = a_dao.search_answer(answers[i]);
-				for(CorrectAnswersBean ans : a_list ) {
-					//入力された答えと一致するレコードがあり、答えに紐づく問題が一致する場合
-					if(ans.getId() != 0 && ans.getQuestionsId() == questions_id[i]) {
+			//questionsIdの要素数分だけ処理を繰り返す
+			for(int i = 0; i < questionsId.length; i++) {
+				//CorrectAnswersDaoオブジェクトを作成
+				CorrectAnswersDao ansDao = new CorrectAnswersDao();
+				//search_answerメソッドを呼び出して、answerと一致するレコードを取得
+				ArrayList<CorrectAnswersBean> ansList = ansDao.search_answer(answers[i]);
+				//ansListの要素数分だけ繰り返す
+				for(CorrectAnswersBean ans : ansList ) {
+					//入力された答えと一致するレコードがあり、答えに紐づく問題idが一致する場合
+					if(ans.getId() != 0 && ans.getQuestionsId() == questionsId[i]) {
+						//正解の問題数をカウントアップ
 						correctQueCnt ++;
+						//繰り返し処理を抜ける
 						break;
 					}
 				}
 			}
-			//点数を計算
-			int point = Math.round(100 * correctQueCnt / questions_id.length);
+			//100 * 正解の問題数÷全体の問題数を計算し、小数点以下は四捨五入し、点数を計算
+			int point = Math.round(100 * correctQueCnt / questionsId.length);
 			
-			//sessionからuser_idを取得
+			//セッションの取得
 			HttpSession session = request.getSession(false);
+			//セッションからuserIdを取得し、int型に変換し、変数userIdに格納
 			int userId = (int)session.getAttribute("userId");
 			
-			//履歴を登録
-			HistoriesDao h_dao = new HistoriesDao();
-			h_dao.register_history(userId, point);
+			//HisotiresDaoオブジェクトを作成
+			HistoriesDao hisDao = new HistoriesDao();
+			//register_historyメソッドを呼び出し、履歴を登録
+			hisDao.register_history(userId, point);
 			
-			//user_idからuser情報を取得
+			//UsersDaoオブジェクトを作成
 			UsersDao dao = new UsersDao();
+			//sarch_idメソッドを呼び出し、userIdと一致するレコードを取得
 			UsersBean user = dao.search_id(userId);
 			
+			//リクエストスコープへcorrextQueCntを格納
 			request.setAttribute("correctQueCnt", correctQueCnt);
-			request.setAttribute("queCnt", questions_id.length);
+			//リクエストスコープへquestionsIdの要素数（問題数）を格納
+			request.setAttribute("queCnt", questionsId.length);
+			//リクエストスコープへpointを格納
 			request.setAttribute("point", point);
+			//リクエストスコープへユーザー名を格納
 			request.setAttribute("userName", user.getName());
 			
+			//画面の遷移先として、Result画面を定義
 			RequestDispatcher rd = request.getRequestDispatcher("/Result.jsp");
+			//foward(...)で定義された転送先に処理が移る
 			rd.forward(request, response);
 			return;
+		//try文の中で例外が発生した場合、catch句に処理が移る
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
