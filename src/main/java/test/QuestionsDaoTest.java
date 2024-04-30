d0474bd17772411f85e8c96f145ac87486c19797
masuda <r_masuda@emyus.jp>
2024-04-30 13:53:38 +0900

    QuestionsDaoTest において、findAllとregisterメソッドのテストを同じメソッドにまとめる、その他もいくつか修正

--- src/main/java/test/QuestionsDaoTest.java
+++ src/main/java/test/QuestionsDaoTest.java
@@ -7,91 +7,94 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import kensyu.QuestionsBean;
 import kensyu.QuestionsDao;
 
+@TestMethodOrder(OrderAnnotation.class) 
 class QuestionsDaoTest {
 	
 	@Test
+	@Order(1)
+	@DisplayName("register_questionメソッドで、問題が登録でき、findAllメソッドで、登録した問題を取得できること")
+	public void registerQuestion() {
+		try {
+			
+			QuestionsDao dao = new QuestionsDao();
+			ArrayList<QuestionsBean> queList = dao.findAll();
+			String question = "testtest";
+			dao.register_question(question);
+			String question2 = "testtest2";
+			dao.register_question(question2);
+			String question3 = "testtest3";
+			dao.register_question(question3);
+			ArrayList<QuestionsBean> queList2 = dao.findAll();
+			
+			assertThat(queList2.size() - queList.size(), is(3));
+			assertThat(queList2.get(queList2.size()-3).getQuestion(), is("testtest"));
+			assertThat(queList2.get(queList2.size()-2).getQuestion(), is("testtest2"));
+			assertThat(queList2.get(queList2.size()-1).getQuestion(), is("testtest3"));
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	@Order(2)
 	@DisplayName("search_idメソッドに、登録されていないquestionIdを引数として渡した場合、レコードが取得できないこと")
 	public void notGetRecord() {
 		try {
 			QuestionsDao dao = new QuestionsDao();
-			QuestionsBean question = dao.search_id(10);
+			QuestionsBean question = dao.search_id(4);
 			assertThat(question.getId(), is(0));
 			assertNull(question.getQuestion());
-			
 		} catch (Exception e) {
-			e.printStackTrace();
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
 		}
 	}
 	
 	@Test
+	@Order(3)
 	@DisplayName("search_idメソッドに、登録されているquestionIdを引数として渡した場合、レコードを取得できること")
 	public void getRecord() {
 		try {
 			QuestionsDao dao = new QuestionsDao();
-			QuestionsBean question = dao.search_id(1);
-			assertThat(question.getId(), is(1));
-			assertThat(question.getQuestion(), is("test"));
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	@DisplayName("findAllメソッドで、全ての問題が取得できること")
-	public void getAllQuestion() {
-		try {
-			QuestionsDao dao = new QuestionsDao();
 			ArrayList<QuestionsBean> queList = dao.findAll();
-			
-			assertThat(queList.get(0).getId(), is(1));
-			assertThat(queList.get(0).getQuestion(), is("test"));
-			assertThat(queList.get(1).getId(), is(2));
-			assertThat(queList.get(1).getQuestion(), is("test2"));
-			assertThat(queList.get(2).getId(), is(3));
-			assertThat(queList.get(2).getQuestion(), is("test3"));
-			
+			QuestionsBean question = dao.search_id(queList.get(queList.size() - 1).getId());
+	
+			assertThat(question.getQuestion(), is("testtest3"));
 		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
 	}
 	
 	@Test
-	@DisplayName("register_questionメソッドで、問題が登録できること")
-	public void registerQuestion() {
-		try {
-			QuestionsDao dao = new QuestionsDao();
-			String question = "test4";
-			dao.register_question(question);
-			ArrayList<QuestionsBean> queList = dao.findAll();
-			
-			assertThat(queList.size(), is(4));
-			assertThat(queList.get(3).getQuestion(), is("test4"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
+	@Order(4)
 	@DisplayName("delete_questionメソッドで、問題の削除ができること")
 	public void deleteQuestion() {
 		try {
 			QuestionsDao dao = new QuestionsDao();
 			ArrayList<QuestionsBean> queList = dao.findAll();
-			
-			int questionId = queList.get(queList.size() - 1).getId();
-			dao.delete_question(questionId);
+			int questionId1 = queList.get(queList.size() - 1).getId();
+			dao.delete_question(questionId1);
+			int questionId2 = queList.get(queList.size() - 2).getId();
+			dao.delete_question(questionId2);
+			int questionId3 = queList.get(queList.size() - 3).getId();
+			dao.delete_question(questionId3);
 			ArrayList<QuestionsBean> queList2 = dao.findAll();
 			
-			assertThat(queList2.size(), is(3));
-			assertThat(queList2.get(queList2.size() - 1).getQuestion(), is(not("test4")));
+			assertThat(queList.size() - queList2.size(), is(3));
+			assertThat(queList2.get(queList2.size() - 1).getQuestion(), is(not("testtest")));
 			
 		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
 	}
