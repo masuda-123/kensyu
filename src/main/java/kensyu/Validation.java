c8c19a0f4fd6f9fd28ff017a3bacfc9d01616023
masuda <r_masuda@emyus.jp>
2024-05-01 17:37:36 +0900

    Validationクラスを追加し、バリデーションチェックをこのクラスに移す

---
+++ src/main/java/kensyu/Validation.java
@@ -0,0 +1,52 @@
+package kensyu;
+
+public class Validation {
+	
+	public String validate(String question, String[] answers) {
+		
+		String errorMessage = "";
+		boolean isNewLine = false;
+		
+		if(question.isEmpty()) { //問題文が空だった場合
+			//エラーメッセージに文字列を追加
+			errorMessage += "※問題を入力してください。";
+			//改行するようtrueを格納
+			isNewLine = true;
+		} else if(question.length() > 500) { //問題文の文字数が500文字を超えていた場合
+			//エラーメッセージに文字列を追加
+			errorMessage += "※問題の文字数が制限（500文字）を超えています。";
+			//改行するようtrueを格納
+			isNewLine = true;
+		}
+		
+		if (isNewLine) { // trueだった場合
+			//エラーメッセージにbrタグを追加し、改行させる
+			errorMessage += "<br>";
+		}
+		
+		//答えの数だけ処理を繰り返す
+		for(int i = 0; i < answers.length; i++) {
+			//初期値に戻す
+			isNewLine = false;
+			if(answers[i].isEmpty()) { 	//答えが空だった場合
+				//エラーメッセージに文字列を追加
+				errorMessage += "※答え" + (i+1) + "が未入力です。";
+				//改行するようtrueを格納
+				isNewLine = true;
+			}else if(answers[i].length() > 200) { //答えの文字数が200文字を超えていた場合
+				//エラーメッセージに文字列を追加
+				errorMessage += "※答え" + (i+1) + "の文字数が制限（200文字）を超えています。";
+				//改行するようtrueを格納
+				isNewLine = true;
+			}
+			if (isNewLine) { // trueだった場合
+				//エラーメッセージにbrタグを追加し、改行させる
+				errorMessage += "<br>";
+			}
+		}
+		
+		
+		return errorMessage;
+	}
+
+}
