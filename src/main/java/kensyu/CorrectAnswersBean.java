2740e2e671d64e95533001f5e380a3eda3385a5f
masuda <r_masuda@emyus.jp>
2024-05-02 12:18:41 +0900

    Base、CorrectAnswersBean、CorrectAnswersDaoクラスにコメントを追加

--- src/main/java/kensyu/CorrectAnswersBean.java
+++ src/main/java/kensyu/CorrectAnswersBean.java
@@ -10,7 +10,7 @@ public class CorrectAnswersBean {
 	private Timestamp updated_at;
 
 	/**
-	 * コンストラクタ
+	 * コンストラクタで、id、question_id、answerに値をセット
 	 */
 	public CorrectAnswersBean (int id, int questions_id, String answer){
 			this.id = id;
@@ -20,36 +20,54 @@ public class CorrectAnswersBean {
 	
 	/** 引数無しのコンストラクタ **/
 	public CorrectAnswersBean() {
-
 	}
-
+	
+	//メンバ変数のidを取得
 	public int getId() {
 		return this.id;
 	}
+	
+	//メンバ変数のidに値をセット
 	public void setId(int id) {
 		this.id = id;
-	} 
+	}
+	
+	//メンバ変数のquestions_Idを取得
 	public int getQuestionsId() {
 		return this.questions_id;
-	}	
+	}
+	
+	//メンバ変数のquestion_idに値をセット
 	public void setQuestionsId(int questions_id) {
 		this.questions_id = questions_id;
 	}
+	
+	//メンバ変数のanswerを取得
 	public String getAnswer() {
 		return this.answer;
-	}	
+	}
+	
+	//メンバ変数のanswerに値をセット
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
+	
+	//メンバ変数のcreated_atを取得
 	public Timestamp getCreatedAt() {
 		return this.created_at;
 	}
+	
+	//メンバ変数のcreated_atに値をセット
 	public void setCreatedAt(Timestamp created_at) {
 		this.created_at = created_at;
 	}
+	
+	//メンバ変数のupdated_atを取得
 	public Timestamp getUpdatedAt() {
 		return this.updated_at;
 	}
+	
+	//メンバ変数のupdated_atに値をセット
 	public void setUpdatedAt(Timestamp updated_at) {
 		this.updated_at = updated_at;
 	}
