b69ecb66c116bfc8a983caed6047f1264974fae7
masuda <r_masuda@emyus.jp>
2024-05-02 16:41:08 +0900

    HistoriesDaoTestクラスにコメントを追加

--- src/main/java/test/HistoriesDaoTest.java
+++ src/main/java/test/HistoriesDaoTest.java
@@ -14,16 +14,22 @@ import kensyu.HistoriesDao;
 class HistoriesDaoTest {
 
 	@Test
-	@DisplayName("register_historyメソッドで、答えが登録できること")
+	@DisplayName("register_historyメソッドで、履歴が登録できること")
 	void registerHistory() throws Exception {
+		//登録されているuser_idを変数に格納
 		int userId = 1;
 		int point = 77;
 		HistoriesDao hisDao = new HistoriesDao();
+		//渡したuser_idと一致する履歴データを取得
 		ArrayList<HistoriesBean> hisList= hisDao.search_userId(userId);
+		//履歴を登録
 		hisDao.register_history(userId, point);
+		//登録後、再度渡したuser_idと一致する履歴データを取得
 		ArrayList<HistoriesBean> hisList2 = hisDao.search_userId(userId);
-			
+		
+		//登録前と後で、データが増えていることを確認
 		assertThat(hisList2.size() - hisList.size(), is(1));
-		assertThat(hisList.get(hisList.size() - 1).getPoint(), is(77));
+		//細心の履歴データが、先程登録した内容であることを確認
+		assertThat(hisList2.get(hisList2.size() - 1).getPoint(), is(77));
 	}
 }
