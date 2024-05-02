ad40b3333142aa1e14fbacfea3a030b3c1779362
masuda <r_masuda@emyus.jp>
2024-05-02 16:15:30 +0900

    UsersDaoTestクラスにコメントを追加

--- src/main/java/test/UsersDaoTest.java
+++ src/main/java/test/UsersDaoTest.java
@@ -16,7 +16,10 @@ class UsersDaoTest {
 	@DisplayName("search_idメソッドに、登録されていないuserIdを引数として渡した場合、ユーザーが取得できないこと")
 	public void notGetUser() throws Exception {
 		UsersDao dao = new UsersDao();
+		//登録されていないidを渡して、ユーザーを取得しようとする
 		UsersBean user = dao.search_id(4);
+		
+		//答えが取得できないことを確認
 		assertThat(user.getId(), is(0));
 		assertNull(user.getPassword());
 		assertNull(user.getName());
@@ -26,7 +29,10 @@ class UsersDaoTest {
 	@DisplayName("search_idメソッドに、登録されているuserIdを引数として渡した場合、ユーザーを取得できること")
 	public void getUser() throws Exception {
 		UsersDao dao = new UsersDao();
+		//登録されているidを渡して、ユーザーを取得
 		UsersBean user = dao.search_id(1);
+		
+		//ユーザーが取得できることを確認
 		assertThat(user.getId(), is(1));
 		assertThat(user.getName(), is("testuser"));
 		assertThat(user.getPassword(), is("testtest"));
